22dff96afb6860aa4fd83f6b027f77258969cbb2
LuisGrZam <lf.granadoszambrano@ugto.mx>
2021-04-28 13:56:20 -0500

    confirmado

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,93 @@
 #include <Arduino.h>
-#define btnIN A0 //Entrada analogica
-#define LED 10   //Salida digital
+#define btnIN A1    //Entrada analógica (Variable global)
+#define LED 10      //Salida de un LED, el 10 es el pin de salida (Variable global)
 
-enum funMode
-{
+enum FunMode {
   OFF,
   BLINK1,
   BLINK2,
   BLINK3,
-  BLINK4,
-  
 };
 
-int led=10;
-int t=4000;//4seg  
-int t1=4000;//4seg
-unsigned long ta=0;
-unsigned long tb=0;
-bool stado=true;
+int LecturadeBoton();
+int BotondeSeleccion();
+int FuncionOff();
+int FuncionBlink1();
+int FuncionBlink2();
+int FuncionBlink3();
+
+
+unsigned long lastDeTime = 0;
+int LstBSTD = HIGH;    //Último estado predefindo en alto
+int btnSTD = 0;     //Estado presente
+int funcSTD = 0;
+
 void setup() {
-pinMode(led,OUTPUT);
-digitalWrite(led,1);
+  pinMode(btnIN, INPUT_PULLUP);   //Declarar entrada analógica como alto como bajo de manera indefinida
+  pinMode(LED, OUTPUT);           //Declarar salida digital
+
+  // put your setup code here, to run once:
 }
+
 void loop() {
-if(millis()>=t+ta&&stado==true)
-{
- stado=false;
- digitalWrite(led,0);
- tb=millis();
+  LecturadeBoton();
+  BotondeSeleccion();
 }
-if(millis()>=t1+tb&&stado==false)
-{
- stado=true;
- digitalWrite(led,1);
- ta=millis();
- }
+
+int LecturadeBoton(){
+   int reading = digitalRead(btnIN);   //Variable local de lectura
+  if(reading != LstBSTD){              //Último estado definido del boton
+    if((millis()-lastDeTime) > 100){
+    if(reading != btnSTD){
+      btnSTD = reading;
+  }
+  if(btnSTD == HIGH){
+    funcSTD += 1;
+  }
+  }
+  }
+LstBSTD = reading;
+}
+
+int BotondeSeleccion(){
+  switch (funcSTD){
+    case OFF:
+    FuncionOff();
+    break;
+    case BLINK1:
+    FuncionBlink1();  
+    break;
+    case BLINK2:
+    FuncionBlink2();
+    break;
+    case BLINK3:
+    FuncionBlink3();
+    break;
+  }
+}
+
+int FuncionOff(){
+ digitalWrite(LED,LOW);
+}
+
+int FuncionBlink1(){
+  delay(100);
+  digitalWrite(LED,LOW);
+  delay(100);
+  digitalWrite(LED,HIGH);
+}
+
+int FuncionBlink2(){
+    delay(50);
+    digitalWrite(LED,LOW);
+    delay(50);
+    digitalWrite(LED,HIGH);
+}
+
+int FuncionBlink3(){
+    delay(200);
+    digitalWrite(LED,LOW);
+    delay(200);
+    digitalWrite(LED,HIGH);
+    \\Cagas
 }
