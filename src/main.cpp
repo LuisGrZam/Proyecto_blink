d6a4628ef5964708dd5ccc9d421733323367365a
LuisGrZam <lf.granadoszambrano@ugto.mx>
2021-05-14 22:09:32 -0500

    arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,123 @@
 #include <Arduino.h>
-#define btnIN A1    //Entrada analógica (Variable global)
-#define LED 10      //Salida de un LED, el 10 es el pin de salida (Variable global)
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-enum FunMode {
-  OFF,
-  BLINK1,
-  BLINK2,
-  BLINK3,
-};
+int upButton = 10;
+int downButton = 9;
+int selectButton = 8;
 
-int LecturadeBoton();
-int BotondeSeleccion();
-int FuncionOff();
-int FuncionBlink1();
-int FuncionBlink2();
-int FuncionBlink3();
-int EncenderLEDGradual();
+int menu = 1;
+
+int LED = 13;
 
 
-unsigned long lastDeTime = 0;
-int LstBSTD = HIGH;    //Último estado predefindo en alto
-int btnSTD = 0;     //Estado presente
-int funcSTD = 0;
-int x=0; //Control de Intensidad del LED
+
+// Void Setup
 
 void setup() {
-  pinMode(btnIN, INPUT_PULLUP);   //Declarar entrada analógica como alto como bajo de manera indefinida
-  pinMode(LED, OUTPUT);           //Declarar salida digital
+  lcd.begin(16, 2);
 
-  // put your setup code here, to run once:
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(selectButton, INPUT_PULLUP);
+  
+  pinMode(LED, OUTPUT);
+
+  updateMenu();
 }
 
+
+// Void Loop
 void loop() {
-  LecturadeBoton();
-  BotondeSeleccion();
-  EncenderLEDGradual();
-}
-
-int LecturadeBoton(){
-   int reading = digitalRead(btnIN);   //Variable local de lectura
-  if(reading != LstBSTD){              //Último estado definido del boton
-    if((millis()-lastDeTime) > 100){
-    if(reading != btnSTD){
-      btnSTD = reading;
+  if (!digitalRead(downButton)) {
+    menu++;
+    updateMenu();
+    delay(100);
+    while (!digitalRead(downButton));
   }
-  if(btnSTD == HIGH){
-    funcSTD += 1;
+
+  if (!digitalRead(upButton)) {
+    menu--;
+    updateMenu();
+    delay(100);
+    while (!digitalRead(upButton));
   }
+
+  if (!digitalRead(selectButton)) {
+    execute();
+    updateMenu();
+    delay(100);
+    while (!digitalRead(selectButton));
+  }  
+}
+
+
+// Actualizaciones
+void updateMenu() {
+  switch (menu) {
+    case 0:
+      menu = 1;
+      break;
+    case 1:
+      lcd.clear();
+      lcd.print("Encender LED");
+    
+      break;
+    case 2:
+      lcd.clear();
+      lcd.print(">Apagar LED");
+
+      break;
+
   }
-  }
-LstBSTD = reading;
 }
 
-int BotondeSeleccion(){
-  switch (funcSTD){
-    case OFF:
-    FuncionOff();
-    break;
-    case BLINK1:
-    FuncionBlink1();  
-    break;
-    case BLINK2:
-    FuncionBlink2();
-    break;
-    case BLINK3:
-    FuncionBlink3();
-    break;
+
+// Ejecutar Casos 
+
+void execute() {
+  switch (menu) {
+    case 1:
+      action1();
+      break;
+    case 2:
+      action2();
+      break;
+   
   }
 }
 
-int FuncionOff(){
- digitalWrite(LED,LOW);
+
+// Accion
+
+void action1() {
+  lcd.clear();
+  lcd.print(">Encender LED");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  
+  digitalWrite(LED, HIGH);
+  
+  delay(500);
 }
 
-int FuncionBlink1(){
-  delay(100);
-  digitalWrite(LED,LOW);
-  delay(100);
-  digitalWrite(LED,HIGH);
+void action2() {
+  lcd.clear();
+  lcd.print(">Apagar LED");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  
+  digitalWrite(LED, LOW);
+  
+  delay(500);
 }
 
-int FuncionBlink2(){
-    delay(50);
-    digitalWrite(LED,LOW);
-    delay(50);
-    digitalWrite(LED,HIGH);
-}
-
-int FuncionBlink3(){
-    delay(200);
-    digitalWrite(LED,LOW);
-    delay(200);
-    digitalWrite(LED,HIGH);
-}
-
-int EncenderLEDGradual(){
- for(x=0;x<256; x+=5){ //Este ciclo es para encender gradualmente
- analogWrite(LED,x);   //Encender LED con intensidad establecida en x
- x = millis();
- }
-for(x=0;x<256; x-=5){ //Este ciclo es para apagar gradualmente
- analogWrite(LED,x);   //Encender LED con intensidad establecida en x
- x = millis();
-}
-}
