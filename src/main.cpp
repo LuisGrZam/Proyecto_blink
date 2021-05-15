9e103d197015d6187bc44201d0fbd667520b3580
LuisGrZam <lf.granadoszambrano@ugto.mx>
2021-05-14 23:06:50 -0500

    cambio

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <Wire.h>
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -23,8 +24,7 @@ void setup() {
   pinMode(selectButton, INPUT_PULLUP);
   
   pinMode(LED, OUTPUT);
-
-  updateMenu();
+  void updateMenu();
 }
 
 
