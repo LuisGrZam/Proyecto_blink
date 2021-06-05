8eb064e72b1f224367980aa3df486045c09fbf46
LuisGrZam <lf.granadoszambrano@ugto.mx>
2021-06-04 23:56:24 -0500

    todo

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,38 @@
-#include <Arduino.h>
-#include <LiquidCrystal.h>
+//Este es el código correspondiente al emisor 
+#include <SPI.h>   //Librería de protocolo de comunicación serial
+//#include <nRF24L01.h> //Librería correspondiente al módulo de antena
+#include <RF24.h> //Librería correspondiente al módulo de antena
+#include <DHT11.h> //Librería de módulo de sensor de temperatura y humedad
 #include <Wire.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-int upButton = 10;
-int downButton = 9;
-int selectButton = 8;
-int menu = 1;
-int LED = 13;
-int EncenderLEDGradual();
-int x=0;
-
-
-
-// Void Setup
+ 
+int pin = A0;   //Declaración de pin A0
+DHT11 dht11(pin);
+float temperature[2];
+ 
+double Fahrenheit(double celsius) {
+return ((double)(9 / 5) * celsius) + 32;
+}
+ 
+double Kelvin(double celsius) {
+return celsius + 273.15;
+}
+ 
+RF24 radio(8, 9);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+float temp, humi;
 
 void setup() {
-  lcd.begin(16, 2);
-
-  pinMode(upButton, INPUT_PULLUP);
-  pinMode(downButton, INPUT_PULLUP);
-  pinMode(selectButton, INPUT_PULLUP);
-  
-  pinMode(LED, OUTPUT);
-
-  updateMenu();
+Serial.begin(9600);
+radio.begin();
+radio.openWritingPipe(pipe);
 }
-
-
-// Void Loop
-void loop() {
-  if (!digitalRead(downButton)) {
-    menu++;
-    updateMenu();
-    delay(100);
-    while (!digitalRead(downButton));
-  }
-
-  if (!digitalRead(upButton)) {
-    menu--;
-    updateMenu();
-    delay(100);
-    while (!digitalRead(upButton));
-  }
-
-  if (!digitalRead(selectButton)) {
-    execute();
-    updateMenu();
-    delay(100);
-    while (!digitalRead(selectButton));
-  }	
-}
-
-
-// Actualizaciones
-void updateMenu() {
-  switch (menu) {
-    case 0:
-      menu = 1;
-      break;
-    case 1:
-      lcd.clear();
-      lcd.print("Encender LED");
-    
-      break;
-    case 2:
-      lcd.clear();
-      lcd.print(">Apagar LED");
-
-      break;
-
-  }
-}
-
-
-// Ejecutar Casos 
-
-void execute() {
-  switch (menu) {
-    case 1:
-      action1();
-      break;
-    case 2:
-      action2();
-      break;
-   
-  }
-}
-
-
-// Accion
-
-void action1() {
-  lcd.clear();
-  lcd.print(">Encender LED");
-  delay(150);
-  lcd.print(".");
-  delay(150);
-  lcd.print(".");
-  delay(150);
-  lcd.print(".");
-  
-  //digitalWrite(LED, HIGH);
-  //delay(500);
  
-  for(x=0;x<256; x+=5){ //Este ciclo es para encender gradualmente
- analogWrite(LED,x);   //Encender LED con intensidad establecida en x
-delay(70);;
- }
+void loop(void)
+{
 
-
+dht11.read(humi, temp);
+temperature[0] = temp;
+temperature[1] = humi;
+radio.write(temperature, sizeof(temperature));
+delay (1000);
 }
-void action2() {
-  lcd.clear();
-  lcd.print(">Apagar LED");
-  delay(150);
-  lcd.print(".");
-  delay(150);
-  lcd.print(".");
-  delay(150);
-  lcd.print(".");
-  
- 
-  
-  //digitalWrite(LED, LOW);
- for(x=0;x<256; x-=5){ //Este ciclo es para apagar gradualmente
- analogWrite(LED,x);   //Encender LED con intensidad establecida en x
-delay(70);
-}
-}
-
-
